7cf446a0be57d1a06c987e11d4b5037a5409684a
Peter Reutemann <fracpete@gmail.com>
2024-07-05 11:37:35 +1200

    prepared for release

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         "ldc-openai>=0.0.1",
         "ldc-pdf>=0.0.2",
     ],
-    version="0.0.1",
+    version="0.0.2",
     author='Peter Reutemann',
     author_email='fracpete@waikato.ac.nz',
 )
